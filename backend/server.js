a5dda79a14a47abaf48e5c4b4d787d3a4a97f994
Denali Sharp <denalisharp@Denalis-MacBook-Pro.local>
2024-09-03 13:15:30 -0400

    basic layout for signup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
+import logger from "./logger.js";
+import auth from "./routes/auth.js";
 
 const app = express();
+app.use(logger);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
+app.use("/api/auth", auth);
 app.get("/api/status", (req, res) => {
   res.json({ status: "ok" });
 });
